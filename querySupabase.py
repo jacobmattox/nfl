a4684030a51f55ba482dd5d9bf6ebe1a729306d8
Jacob R. Mattox <jacob.mattox1017@gmail.com>
2025-07-03 16:14:10 -0400

    testing db connection

---
+++ querySupabase.py
@@ -0,0 +1,13 @@
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+response = (
+    supabase.table("teams")
+    .select("*")
+    .execute()
+)
+print(response.data, type(response.data[0]))
